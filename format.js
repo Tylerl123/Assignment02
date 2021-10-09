ea5c14b30bb75cea4aabf118d4fd986c1107c1c7
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-09 19:34:26 -0400

    Create format.js

---
+++ format.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+  
+  document.querySelector("#new-task").onsubmit =function(){
+    
+    const li = document.createElement('li');
+    
+    let task_text = document.querySelector('#task').value;
+    let new_task_html =` 
+    
+                         <span> ${task_text} </span>
+                         <button class = "remove"> Remove </button>
+                        `;
+    li.innerHTML = new_task_html
+    
+    document.querySelector("#task_list").append(li);
+    document.querySelector("#task").value = '';
+    
+    return false;
+  }
+  
+  doucment.addEventListener('click', function(event){
+    element = event.target;
+    if(element.className ==== 'remove'){
+      element.parentElement.remove();
+    }
+  })
+  
+});
+  
