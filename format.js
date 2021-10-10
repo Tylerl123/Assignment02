c43ce8009f2796036e4eff4519928537c0b23155
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-10 18:31:19 -0400

    Update format.js

--- format.js
+++ format.js
@@ -2,29 +2,34 @@ alert ('niggga');
 
 document.addEventListener('DOMContentLoaded', function() {
  
- 
+ let the_task = [];
+ the_task.tostring = () => {
+  array.forEach(element => {
+   return `element.innerHTML\n`
+  });
+ }
  
  
  
  
   document.querySelector("#new-task").onsubmit = function a() {
     
-    const li = document.createElement('li');
+    let li = document.createElement('li');
     
     let task_text = document.querySelector('#task').value;
     let task_li = document.querySelector('#priorty').value;
     
-    let new_task_html = ` 
+    li innerHTML = ` 
                          <span> ${task_text} </span> 
                          
                          <button class ="remove">Remove</button>
                          <h4> ${task_li} </h4> <br>
                         
                         `;
-    li.innerHTML = new_task_html
+    
     
     document.querySelector("#task_list").append(li);
-    document.querySelector("#task").value = '';
+    the_task.push(li)
     
      return false;
     
@@ -40,7 +45,9 @@ document.addEventListener('DOMContentLoaded', function() {
    
     element = event.target;
     if (element.className === 'remove') {
-      element.parentElement.remove();
+        element.parentElement.remove();
+        index= the_task.indexOf(element.parentElement);
+        the_task.splice(index, 1)
     }
     
    
