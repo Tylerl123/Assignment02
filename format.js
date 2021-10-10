2680bf75880d541053afaf47b9f275bfa300e8cb
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-10 17:43:38 -0400

    Update format.js

--- format.js
+++ format.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const li = document.createElement('li');
     
     let task_text = document.querySelector('#task').value;
-    let task_list = document.querySelector('#priorty').value;
+    let task_li = document.querySelector('#priorty').value;
     
     let new_task_html = ` 
                          <span> ${task_text} </span> 
                          
                          <button class ="remove">Remove</button>
-                         <h4> ${task_list} </h4> <br>
+                         <h4> ${task_li} </h4> <br>
                         
                         `;
     li.innerHTML = new_task_html
