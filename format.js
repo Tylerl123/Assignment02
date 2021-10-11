b502094092f3a4fd3100218dd45070071f7ff619
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-10 23:05:59 -0400

    Update format.js

--- format.js
+++ format.js
@@ -19,9 +19,10 @@ document.querySelector("#new-task").onsubmit = () => {
   let new_task = document.querySelector('#task').value;
   let new_priorty = document.querySelector('#priorty').value;
   let new_tasks = `
-  <h3 id = "status"> PENDING </h3> <br>
+  
   <h5> ${new_task} </h5> <br>
   <h4> ${new_priorty} </h4> <br>
+  <h3 id = "status"> PENDING </h3> <br>
   <input type = "radio" class = "remove"> <p> Remove from List </p>
   <input type = "radio" class = "completed"> <p> Mark as Completed </p>
   `;
