2e8ae6a7c2f2d278fea414d2f50bc3f47f9ce5b6
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-10 22:51:51 -0400

    Update format.js

--- format.js
+++ format.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+   document.addEventListener('input', function() {
+        if (document.querySelector("#task").value.length == 0) {
+            document.querySelector("#submit").disabled = true;
+        }
+        else {
+            document.querySelector("#submit").disabled = false;
+        }
+    })
+  let the_tasks = [];
+    the_tasks.toString = () => {
+        array.forEach(element => {
+            return `element.innerHTML\n`
+        });
+    }
   
 document.querySelector("#new-task").onsubmit = function () {
-  const li = document.createElement('li');
+  let li = document.createElement('li');
   let new_task = document.querySelector('#task').value;
   let new_priorty = document.querySelector('#priorty').value;
   let new_tasks = `
@@ -16,6 +29,7 @@ document.querySelector("#new-task").onsubmit = function () {
   li.innerHTML = new_tasks
   
   document.querySelector("#task_list").append(li);
+  the_tasks.push(li)
   document.querySelector("#task").value = '';
   return false;
 }
@@ -24,12 +38,15 @@ document.querySelector("#new-task").onsubmit = function () {
   element = event.target;
   if (element.className === 'remove') {
     element.parentElement.remove();
+    index = the_tasks.indexOf(element.parentElement);
+    the_tasks.splice(index,1)
   }
   else if (element.className === 'completed') { 
    document.querySelector("#point").style.textDecoration = "line-through";
    document.querySelector("#status").innerHTML = "COMPLETED";
   }
   
+    document.querySelector("#submit").disabled = true;
 });
 });
 
