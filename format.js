8603424ebdff672e9286528dbf4ac190f4b18066
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-10 21:55:59 -0400

    Update format.js

--- format.js
+++ format.js
@@ -1,52 +1,34 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
- 
- 
-  document.querySelector("#new-task").onsubmit = function () {
-    
-    const li = document.createElement('li');
-    
-    let task_text = document.querySelector('#task').value;
-   // let task_li = document.querySelector('#priorty').value;
-    
-    let new_task_html = ` 
-                         <span> ${task_text} </span> 
-                         
-                         <button class ="remove">Remove</button>
-                         
-                        
-                        `;
-    
-    li.innerHTML = new_task_html
-   
-    document.querySelector("#task_list").append(li);
-    document.querySelector('#task').value = '';
- 
-    
-     return false;
-    
-   
   
+document.querySelector("#new-task").onsubmit = function task_list() {
+  const li = document.createElement('li');
+  let new_task = document.querySelector('#task').value;
+  let new_priorty = document.querySelector('#priorty').value;
+  let new_tasks = `
+  <h3 id = "status"> PENDING </h3> <br>
+  <h5> ${new_task} </h5> <br>
+  <h4> ${new_priorty} </h4> <br>
+  <input type = "radio" class = "remove"> <p> Remove from List </p>
+  <input type = "radio" class = "completed"> <p> Mark as Completed </p>
+  `;
+  
+  li.innerHTML = new_tasks
+  
+  document.querySelector("#task_list").append(li);
+  document.querySelector("#task").value = '';
+  return false;
+}
+
+  document.addEventListener('click', function(event) {
+  element = event.target;
+  if (element.className === 'remove') {
+    element.parentElement.remove();
+  }
+ if (element.className === 'completed') { 
+   document.querySelector("#point").style.textDecoration = "line-through";
+   document.querySelector("#status").innerHTML = "COMPLETED";
   }
   
-  
-
-  
-  document.addEventListener('click', function(event){
-    
-   
-    element = event.target;
-    if (element.className === 'remove') {
-        element.parentElement.remove();
-        
-    }
-    
-   
-  });
-  
-  
-  
-  
 });
-  
+});
+
