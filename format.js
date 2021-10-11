b5a6ebccecb8b2d0fdcdad9d5bdf9de31472505f
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-10 23:24:39 -0400

    Update format.js

--- format.js
+++ format.js
@@ -42,7 +42,8 @@ document.querySelector("#new-task").onsubmit = () => {
     the_tasks.splice(index,1)
   }
   else if (element.className === 'completed') { 
-   document.querySelector("#point").style.textDecoration = "line-through";
+   //document.querySelector("#point").style.textDecoration = "line-through";
+   this.querySelector('.displayed-task').style.textDecoration = (this.querySelector("#status")#status) ? "line-through" : "none";
    document.querySelector("#status").innerHTML = "COMPLETED";
   }
   
